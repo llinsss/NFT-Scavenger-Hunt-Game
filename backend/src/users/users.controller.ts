4eeadbc67cd0cb5e05ea11d993ca9e20d1adfad3
Rukayat Zakariyau <ruke4k4@gmail.com>
2025-02-25 00:53:43 -0800

    update user dtos and endpoint

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,10 +1,11 @@
 /* eslint-disable prettier/prettier */
 import { CreateUserDto } from './dtos/create-user-dto.dto';
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, NotFoundException, Param, ParseIntPipe, Patch, Post, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { AuthTokenGuard } from '../auth/guard/auth-token/auth-token.guard';
 import { Auth } from '../auth/decorators/auth-decorator';
 import { AuthType } from '../auth/enums/auth-type.enum';
+import { UpdateUserDto } from './dtos/update-user-dto';
 
 @Controller('users')
 export class UsersController {
@@ -14,5 +15,18 @@ export class UsersController {
   @Auth(AuthType.None) // Doesn't need protection
   public createUsers(@Body() createUserDto: CreateUserDto) {
     return this.userService.createUser(createUserDto);
+  }
+
+
+  @Patch(':id')
+  public async updateUsers(
+    @Body()updateUserDto:UpdateUserDto, 
+    @Param('id', ParseIntPipe)id:number){
+    const updateUser = await this.userService.findById(id, updateUserDto)
+
+    if(!updateUser){
+      throw new NotFoundException('User was not found')
+    }
+    return updateUser;
   }
 }
--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,8 +1,12 @@
 /* eslint-disable prettier/prettier */
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserProvider } from './providers/create-user-provider.provider';
 import { CreateUserDto } from './dtos/create-user-dto.dto';
 import { FindByUsername } from './providers/find-by-username.provider';
+import { User } from './users.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { UpdateUserDto } from './dtos/update-user-dto';
 
 @Injectable()
 export class UsersService {
@@ -10,6 +14,12 @@ export class UsersService {
     private readonly createUserProvider: CreateUserProvider,
 
     private readonly findByUsername: FindByUsername,
+    
+     /*
+     * inject create user provider
+     */
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
   ) {}
 
   public async createUser(createUserDto: CreateUserDto) {
@@ -18,5 +28,24 @@ export class UsersService {
 
   public async FindByUsername(username: string) {
     return await this.findByUsername.FindOneByUsername(username);
+  }
+  
+  // public async findById(id:number, updateUserDto){
+  //   const user = await this.userRepository.find({where:{id}})
+  //   if(!user){
+  //     throw new NotFoundException('User does not exist')
+  //   }
+  //   Object.assign(user, updateUserDto);
+
+  //   return this.userRepository.save(user);
+  // }
+
+  public async findById(id:number, updateUserDto:UpdateUserDto){
+    const user = await this.userRepository.findOne({where:{id}})
+    if(!user){
+      throw new NotFoundException('No user was found')
+    }
+    Object.assign(user, updateUserDto)
+    return this.userRepository.save(user)
   }
 }
