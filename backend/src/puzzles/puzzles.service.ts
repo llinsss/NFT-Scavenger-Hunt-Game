d508bed42d09f6e732fe493189a7ba4be4fdb557
KuchiMercy <durumercy206@gmail.com>
2025-02-25 11:15:24 +0100

    Removed the function in the puzzle.services.ts and reverted the file

--- backend/src/puzzles/puzzles.service.ts
+++ backend/src/puzzles/puzzles.service.ts
@@ -1,21 +1,3 @@
-import { Injectable, BadRequestException } from '@nestjs/common';
-import { LevelEnum } from 'src/enums/LevelEnum';
+import { Injectable } from '@nestjs/common';
 @Injectable()
-export class PuzzlesService {
-  private puzzles: { id: number; level: LevelEnum }[] = [];
-
-  createPuzzle(id: number, level: LevelEnum) {
-    if (!Object.values(LevelEnum).includes(level)) {
-      throw new BadRequestException(`Invalid level: ${level}`);
-    }
-    const newPuzzle = {
-      id: this.puzzles.length + 1,
-      level: level,
-    };
-    this.puzzles.push(newPuzzle);
-    return newPuzzle;
-  }
-  getAllPuzzles() {
-    return this.puzzles;
-  }
-}
+export class PuzzlesService {}
