a8473c23c06824d51b1ebf087a3a49d4393af16e
Crood <crood097@gmail.com>
2025-02-25 20:44:14 +0530

    Update nfts.controller.ts

--- backend/src/nfts/nfts.controller.ts
+++ backend/src/nfts/nfts.controller.ts
@@ -1,18 +1,4 @@
-import { Body, Controller, Post, Put, Param } from '@nestjs/common';
-import { CreateNFTDto, UpdateNFTDto } from './nft.dto';
-import { NFTsService } from './nfts.service';
+import { Controller } from '@nestjs/common';
 
 @Controller('nfts')
-export class NFTsController {
-  constructor(private readonly nftsService: NFTsService) {}
-
-  @Post()
-  create(@Body() createNFTDto: CreateNFTDto) {
-    return this.nftsService.create(createNFTDto);
-  }
-
-  @Put(':id')
-  update(@Param('id') id: number, @Body() updateNFTDto: UpdateNFTDto) {
-    return this.nftsService.update(id, updateNFTDto);
-  }
-}
+export class NftsController {}
