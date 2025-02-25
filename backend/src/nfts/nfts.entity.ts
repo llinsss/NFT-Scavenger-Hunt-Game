cdfba86a020c659512798f4c326e7d5fe3cbe423
Crood <crood097@gmail.com>
2025-02-25 20:46:45 +0530

    Update nfts.entity.ts

--- backend/src/nfts/nfts.entity.ts
+++ backend/src/nfts/nfts.entity.ts
@@ -1,12 +1,15 @@
-import { Puzzles } from 'src/puzzles/puzzles.entity';
-import { Entity, PrimaryGeneratedColumn, OneToOne, JoinColumn } from 'typeorm';
+import { Puzzles } from "src/puzzles/puzzles.entity"
+import { Entity, PrimaryGeneratedColumn, Column, OneToOne, JoinColumn } from "typeorm"
 
 @Entity()
 export class NFTs {
   @PrimaryGeneratedColumn()
-  id: number;
+  id: number
 
-  @OneToOne(() => Puzzles, (puzzles) => puzzles.nfts)
+  @OneToOne(
+    () => Puzzles,
+    (puzzles) => puzzles.nfts,
+  )
   @JoinColumn()
-  puzzles: Puzzles;
+  puzzles: Puzzles
 }
