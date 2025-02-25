fa504305adda22f8eca1d10c7a4286f8374c470a
Shukazuby <shukazuby@gmail.com>
2025-02-25 06:21:41 +0100

    removed Puzzle entity

--- backend/src/hints/hints.module.ts
+++ backend/src/hints/hints.module.ts
@@ -3,11 +3,10 @@ import { HintsController } from './hints.controller';
 import { HintsService } from './hints.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Hints } from './hints.entity';
-import { Puzzles } from 'src/puzzles/puzzles.entity';
 import { PuzzlesService } from 'src/puzzles/puzzles.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Hints, Puzzles])],
+  imports: [TypeOrmModule.forFeature([Hints])],
   controllers: [HintsController],
   providers: [HintsService, PuzzlesService],
   exports: [HintsService],
