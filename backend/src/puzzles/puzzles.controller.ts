cb4a248bb032f6e3ed6557ebfcfe34bfda9172cd
Electrifier <samchijioke16@gmail.com>
2025-02-25 19:13:49 +0100

    feat: Add CreatePuzzleDto and puzzle creation controller

--- backend/src/puzzles/puzzles.controller.ts
+++ backend/src/puzzles/puzzles.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Post } from '@nestjs/common';
+import { validate } from 'class-validator';
+import { CreatePuzzleDto } from './dtos/createPuzzles.dto';
+import { PuzzlesService } from './puzzles.service';
+
 
 @Controller('puzzles')
-export class PuzzlesController {}
+export class PuzzlesController {
+    constructor(private readonly puzzleService: PuzzlesService) {}
+
+    @Post()
+    async create(@Body() createPuzzleDto: CreatePuzzleDto) {
+        const errors = await validate(createPuzzleDto)
+        if (errors.length > 0) {
+            throw new BadRequestException(errors);
+        }
+
+        return this.puzzleService.create(createPuzzleDto);
+    }
+}
