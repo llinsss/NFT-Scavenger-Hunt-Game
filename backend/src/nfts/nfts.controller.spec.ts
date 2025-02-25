55a0b4fdf65621bc2f12df330630c43e58632530
Crood <crood097@gmail.com>
2025-02-25 20:41:50 +0530

    Update nfts.controller.spec.ts

--- backend/src/nfts/nfts.controller.spec.ts
+++ backend/src/nfts/nfts.controller.spec.ts
@@ -1,18 +1,4 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { NFTsController } from './nfts.controller';
+import { Controller } from '@nestjs/common';
 
-describe('NftsController', () => {
-  let controller: NFTsController;
-
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [NFTsController],
-    }).compile();
-
-    controller = module.get<NFTsController>(NFTsController);
-  });
-
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
-});
+@Controller('nfts')
+export class NftsController {}
