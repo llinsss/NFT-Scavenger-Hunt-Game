248ef9b2781801aee63cef884464501eb3b00a85
BravarDev <brayanvafer@gmail.com>
2025-02-28 19:38:23 -0600

    feat/ finish basic crud operations
    Wiht this I finished the crud methos in ScoresService
    Also I add a dto for update and set one property for the create dto.
    Fix some variables in entity

--- backend/src/scores/scores.service.ts
+++ backend/src/scores/scores.service.ts
@@ -5,6 +5,11 @@ import { Repository } from 'typeorm';
 import { Scores } from './scores.entity';
 import { PuzzlesService } from 'src/puzzles/puzzles.service';
 import { ScoreResponseDto } from './dto/response-score.dto';
+import { CreateScoreDto } from './dto/create-score.dto';
+import { async } from 'rxjs';
+import { UsersService } from 'src/users/users.service';
+import { UserProgressService } from 'src/user-progress/user-progress.service';
+import { UpdateScoreDto } from './dto/update-score.dto';
 
 @Injectable()
 export class ScoresService {
@@ -14,62 +19,75 @@ export class ScoresService {
     @InjectRepository(Scores)
     private  scoresRepository: Repository<Scores>,
 
-    //deine repository injection for user entity
-    @InjectRepository(User)
-    private  userRepository: Repository<User>,
+    //deine dependecy injection for user service
+    private readonly userService : UsersService,
 
     //define dependency injection for puzzle Service
     private readonly puzzleService: PuzzlesService,
+
+    //define dependency injection for user progress Service
+    private readonly userProgressService: UserProgressService
   ) {}
 
   /**
    * A function to retrieve scores from DB
    * @returns an array with all Scores
    */
-  async getAllScores() : Promise<ScoreResponseDto[]> {
-    const scores: Scores[] = await this.scoresRepository.find();
-    const scoresDto: ScoreResponseDto[] = scores.map(item => {
-      return ScoreResponseDto.map(item);
+  async getAllScores(){
+    return await this.scoresRepository.find();
+  }
+
+  async getById(id:number){
+    return await this.scoresRepository.findOne({where: {id}});
+  }
+
+  async createScore(dto: CreateScoreDto){
+    const user = await this.userService.FindByUsername(dto.username);
+    if (!user) throw new Error("User not found");
+    
+    const puzzle = await this.puzzleService.getAPuzzle(dto.puzzleId);
+    if (!puzzle) throw new Error("Puzzle not found");
+    
+    const userProgress = await this.userProgressService.getUserProgress(user.id); //returns an array, scores expect one item
+    if (!userProgress) throw new Error("User progress not found");
+
+    // TODO: Fix the relation between userProgress and scores so that the create method is properly formated
+    const score = this.scoresRepository.create({
+        user,
+        score: dto.score,
+        puzzle,
+        userProgress
     });
-    return scoresDto;
+    return await this.scoresRepository.save(score);
   }
 
-  async getById(id:number): Promise<ScoreResponseDto>{
-    return ScoreResponseDto.map(await this.scoresRepository.findOne({where: {id}}));
+  async updateScore(id: number, dto: UpdateScoreDto): Promise<Scores | null> {
+    const score = await this.scoresRepository.findOne({ where: { id } });
+    if (!score) throw new Error("Score not found");
+
+    score.score = dto.score;
+    return await this.scoresRepository.save(score);
   }
 
-  async createScore(score: ScoreCreationDTO) {
-
+  async deleteScore(id: number): Promise<void> {
+    await this.scoresRepository.delete(id);
   }
-
-
 
   //fetch leaderboard with pagination
-  async getLeaderboard(page: number = 1, limit: number = 10) {
-    const [users, total] = await this.userRepository.findAndCount({
-      order: { scores: 'DESC', update_at: 'ASC' },
-      skip: (page - 1) * limit,
-      take: limit,
-    });
+  //TODO -> Fix the function to use userService, it provides more order
+  // async getLeaderboard(page: number = 1, limit: number = 10) {
+  //   const [users, total] = await this.userRepository.findAndCount({
+  //     order: { scores: 'DESC', update_at: 'ASC' },
+  //     skip: (page - 1) * limit,
+  //     take: limit,
+  //   });
 
-    return {
-      data: users,
-      currentPage: page,
-      totalPages: Math.ceil(total / limit),
-      totalItems: total,
-    };
-  }
+  //   return {
+  //     data: users,
+  //     currentPage: page,
+  //     totalPages: Math.ceil(total / limit),
+  //     totalItems: total,
+  //   };
+  // }
 
-  // Update or insert user score
-  async updateScore(username: string, score: number) {
-    let user = await this.userRepository.findOne({ where: { username } });
-    if (user) {
-      user.score = score;
-      // update score if user exists
-    } else {
-      user = this.userRepository.create({ username, score });
-      //create a new user if not found
-    }
-    return this.userRepository.save(user);
-  }
 }
